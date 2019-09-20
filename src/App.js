fb884210a5d2a61491e70cd6cb6f2fc88d1b266b
Anton Shevchenko <Anton_Shevchenko5@epam.com>
2019-09-20 18:43:33 +0300

    d3 experiments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 import React from 'react';
 import GameField from "./components/Field";
-
+import * as d3 from "d3";
 import './App.css';
+import {BASE_CELL_MARGIN, BASE_CELL_SIZE} from "./constants";
+import Field from "./classes/Field";
+
+
 
 export default class App extends React.Component {
+    componentWillMount() {
+        let width = 10 ,height = 10;
+        const field = new Field(width, height);
+        const svg = d3.select('#root')
+            .append('svg')
+            .attr('width', (BASE_CELL_SIZE + 2 * BASE_CELL_MARGIN) * width)
+            .attr('height', (BASE_CELL_SIZE + 2 * BASE_CELL_MARGIN) * height)
+            .attr('fill', d3.color('#dedede'))
+            .append('g')
+
+        function dragstarted(d) {
+            d3.select(this).raise().attr("stroke", "black");
+        }
+
+        function dragged(d) {
+            d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+        }
+
+        function dragended(d) {
+            d3.select(this).attr("stroke", null);
+        }
+
+        const dragListeners = d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
+
+        svg.selectAll("rect")
+            .data(field.fieldMap)
+            .enter()
+            // .append('g')
+            .selectAll("circle")
+            .data((d,i) => {d.colNum = i; return d;})
+            .enter()
+            .append("circle")
+            .attr('fill', d => d3.color(d.color.hex))
+            .attr("r", BASE_CELL_SIZE / 2)
+            .attr("cx", (d) => d.col * BASE_CELL_SIZE + BASE_CELL_SIZE / 2)
+            .attr("cy", function(d, i) {
+                return BASE_CELL_SIZE  / 2 + d.row * BASE_CELL_SIZE;
+            });
+
+        svg.selectAll('circle')
+            .call(dragListeners)
+    }
+
     render() {
         return (
             <div className="App">
-              <GameField cols={10} rows={10} />
+                <GameField cols={10} rows={10} />
             </div>
         );
     }
--- src/components/Column/index.js
+++ src/components/Column/index.js
@@ -7,8 +7,9 @@ import {BASE_CELL_MARGIN, BASE_CELL_SIZE} from "../../constants";
 export default class Column extends React.Component {
     render() {
         const { col, colIndex } = this.props;
+        const colWidth = BASE_CELL_SIZE + 2 * BASE_CELL_MARGIN;
         return (
-            <div className="Column" style={{width: `${BASE_CELL_SIZE + 2 * BASE_CELL_MARGIN}px`}}>
+            <div className="Column" style={{width: `${colWidth}px`}}>
                 {col.map((cell, i) => <GameCell key={i} cell={cell} row={i} col={colIndex}  />)}
             </div>
         );
